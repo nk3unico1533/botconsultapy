646282652a064625524b4c234e4bdd8d463aa693
nk3unico1533 <nk3unico@outlook.com>
2025-11-20 08:43:09 -0300

    Atualizar o bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,15 +1,5 @@
-from telegram.ext import ApplicationBuilder
 from app.config import Config
-from app.handlers.start import start_handler
-from app.handlers.menu import menu_handler
-from app.handlers.consultas import consulta_message_handler
 
 def create_bot():
-    app = ApplicationBuilder().token(Config.BOT_TOKEN).build()
-
-    # Handlers centrais
-    app.add_handler(start_handler)
-    app.add_handler(menu_handler)
-    app.add_handler(consulta_message_handler)
-
-    return app
+    print("=== TOKEN NO RUNTIME ===")
+    print("TOKEN:", repr(Config.BOT_TOKEN))
